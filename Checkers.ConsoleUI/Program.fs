0d079411fdb4d8094896f4d812ddd4c069b20a42
Zach Robinson <zprobinson@sio.midco.net>
2020-10-27 21:03:39 -0500

    created functions to label the board.

--- Checkers.ConsoleUI/Program.fs
+++ Checkers.ConsoleUI/Program.fs
@@ -6,16 +6,15 @@ open Checkers
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    printfn ""
+    printfn "Checkers in F#!\n"
 
-    let gameState =
+    let initGameState =
         Implementation.initGame()
 
-    let board=
-        gameState.Board
+    let initBoard=
+        initGameState.Board
 
-    let printCheckerboard (board: Board) (cell: Cell) = 
+    let getCellValue (board: Board) (cell: Cell) = 
         match board.[cell] with
         | Some checker -> 
             match checker with
@@ -25,15 +24,44 @@ let main argv =
             | Red, King -> " [O]"
         | None -> " [ ]"
 
-    let printCell = printCheckerboard board
+    let printCell = getCellValue initBoard
 
-    let checkersGrid = 
-        [for row in Row.List do
-         for col in Column.List do
-         let cell = { Row = row; Column = col }
-         match col with
-         | H -> printfn " %s" (printCell cell)
-         | _ -> printf " %s" (printCell cell) ]
+    let printRowLabel (row: Row) list =
+        sprintf " %d | " ((list |> List.findIndex(fun r -> r = row)) + 1)
+
+    let checkerBoardList = 
+        let rowRev = Row.List |> List.rev
+        [for row in rowRev do
+             printRowLabel row Row.List
+             for col in Column.List do
+                 let cell = printCell ({ Row = row; Column = col })
+                 match col with
+                 | H -> sprintf "%s\n" cell
+                 | _ -> sprintf "%s"   cell ]
+
+    let checkerBoard =
+        checkerBoardList |> List.reduce (+)
+    
+    let bottomPadding length =
+        String.replicate length " "
+
+    let columnBorderBuilder str list = 
+        [for _ in list do
+            sprintf "%s" str]
+        |> List.reduce (+)
+
+    let columnLabelBuilder list =
+        [for item in list do
+            sprintf " %A  " item]
+        |> List.reduce (+)
+
+    let bottomBorder =
+        bottomPadding 6 + columnBorderBuilder "___ " Column.List + "\n" +
+        bottomPadding 6 + columnLabelBuilder Column.List + "\n\n"
+
+    printf "%s" checkerBoard
+    printf "%s" bottomBorder
+    printfn "Please enter moves in the following format: A3 B4 (case insensitive)"
 
 
 
