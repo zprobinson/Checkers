53c94f68c4488c82df11e55503e9a8ce95e162d0
Zach Robinson <zprobinson@sio.midco.net>
2020-11-07 15:13:28 -0600

    cleaned main program

--- Checkers.ConsoleUI/Program.fs
+++ Checkers.ConsoleUI/Program.fs
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open Checkers
 open CheckerTypes
 open Implementation
@@ -9,30 +7,22 @@ open PrintBoard
 open ParseCharacterInput
         
 let rec renderBoard (gameState : GameState) =
+    //print game board and current message
     printfn "%As turn to move.\n" gameState.ColorToMove
-
-    //print checkerboard
     printf "%s" (printBoard gameState.Board)
-
-    //write gameState message
     printfn "%s" gameState.Message
 
-    //prompt and receive input
+    //prompt user for input
     printfn "%s" printPrompt
     printf "> "
     let input = Console.ReadLine()
 
-    //parse input to AttemptedMove
+    //parse input to AttemptedMove type
     let attempt = createAttemptedMove input
 
-    //validate AttemptedMove and
-    //return new GameState object
+    //validate AttemptedMove and refresh display using new GameState
     let newGameState = updateGame gameState attempt
-
-    //clear console
     Console.Clear()
-
-    //call this method
     renderBoard newGameState
 
 [<EntryPoint>]
@@ -40,6 +30,4 @@ let main argv =
     //renderBoard (initNewGame())
     renderBoard (initMultipleCaptureTest())
     //renderBoard (initWinConditionTest())
-
-
-    0 // return an integer exit code
+    0 
