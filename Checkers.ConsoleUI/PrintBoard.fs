d85bf82155f56a2dd7d241d83db172bc6f08bac2
FatherFoxxy <rattusunison@gmail.com>
2020-11-10 17:09:07 -0800

    Open Checkers instead of Checkers.CheckerTypes

--- Checkers.ConsoleUI/PrintBoard.fs
+++ Checkers.ConsoleUI/PrintBoard.fs
@@ -1,10 +1,9 @@
 ﻿module PrintBoard
+open Checkers
 
-open Checkers.CheckerTypes
-
-let private printCell (board: Board) (cell: Cell) = 
+let private printCell (board: Board) (cell: Cell) =
     match board.[cell] with
-        | Some checker -> 
+        | Some checker ->
             match checker with
             | Black, Soldier -> " [x]"
             | Black, King -> " [X]"
@@ -15,23 +14,23 @@ let private printCell (board: Board) (cell: Cell) =
 let private printRowLabel (row: Row) =
     sprintf " %d | " ((Row.List |> List.findIndex(fun r -> r = row)) + 1)
 
-let private checkerBoardList board = 
+let private checkerBoardList board =
     let rowRev = Row.List |> List.rev
     [for row in rowRev do
         printRowLabel row
         for col in Column.List do
-            let cell = printCell board ({ Row = row; Column = col }) 
+            let cell = printCell board ({ Row = row; Column = col })
             match col with
             | H -> sprintf "%s\n" cell
             | _ -> sprintf "%s"   cell ]
 
 let private checkerBoard board =
     (checkerBoardList board) |> List.reduce (+)
- 
+
 let private bottomPadding length =
     String.replicate length " "
 
-let private columnBorderBuilder str list = 
+let private columnBorderBuilder str list =
     [for _ in list do
         sprintf "%s" str]
     |> List.reduce (+)
@@ -46,8 +45,8 @@ let private bottomBorder =
     bottomPadding 6 + columnLabelBuilder Column.List + "\n\n"
 
 let printBoard (board: Board) =
-    sprintf "%s" (checkerBoard board) + 
+    sprintf "%s" (checkerBoard board) +
     sprintf "%s" bottomBorder
 
-let printPrompt = 
+let printPrompt =
     sprintf "Please enter moves in the following format: A3 B4 (case insensitive)"
